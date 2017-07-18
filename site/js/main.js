cc39e4f16ecd2b7544b4216fbeb9edf1d76c16b6
Filipe Vieira <sv.filipe@gmail.com>
2017-07-18 20:30:11 +0000

    update buttons

--- site/js/main.js
+++ site/js/main.js
@@ -2,31 +2,78 @@ const Navigo = require("navigo");
 
 const router = new Navigo(null, true, '#!');
 
+const cache = {templates: {}};
+
+const content = document.getElementById("content");
+
+function getPage (url) {
+    const html = cache.templates[url];
+
+    if (!html) {
+        return fetch(url).then(function(response) {
+            return response.text();
+        }).then(function (html) {
+            cache.templates[url] = html;
+            return html;
+        });
+    }
+    else {
+        return Promise.resolve(html);
+    }
+}
 
 function open (url) {
-    const content = document.getElementById("content");
-    
-    fetch(url).then(function(response) {
-        return response.text();
-    }).then(function (html) {
+    return getPage(url).then((html) => {
         content.innerHTML = html;
     });
 }
 
+// preload pages,
+[
+    'site/pages/play.html',
+    'site/pages/contribute.html',
+    'site/pages/credits.html',
+    'site/pages/pepperandcarrot.html',
+    'site/pages/about.html'
+].forEach(getPage);
+
+function deselect () {
+    const el = document.querySelector(".selected");
+    
+    if (el) {
+        el.className = el.className.replace("selected", "");
+    }
+}
+
 router
     .on("/play", function () {
+        deselect();
+
+        document.getElementById("play").className += "selected";
         open('site/pages/play.html');
     })
     .on("/contribute", function () {
+        deselect();
+
+        document.getElementById("contribute").className += "selected";
         open('site/pages/contribute.html');
     })
     .on("/credits", function () {
+        deselect();
+
+        document.getElementById("credits").className += "selected";
         open('site/pages/credits.html');
     })
     .on("/pepperandcarrot", function () {
+        deselect();
+
+        document.getElementById("pepperandcarrot").className += "selected";
         open('site/pages/pepperandcarrot.html');
     })
     .on("/", function () {
+        deselect();
+        
+        document.getElementById("root").className += "selected";
         open('site/pages/about.html');
     })
     .resolve();
