1e446729661c61ce38ca7b7d4aea0d61e280b0af
Filipe Vieira <sv.filipe@gmail.com>
2017-08-06 14:22:26 +0000

    optimize page loads

--- site/js/bundle.js
+++ site/js/bundle.js
@@ -574,71 +574,126 @@ const Navigo = require("navigo");
 
 const router = new Navigo(null, true, '#!');
 
-const cache = { templates: {} };
+// const cache = {templates: {}};
 
 const content = document.getElementById("content");
 
 const gallery = require("./gallery");
 
-function getPage(url) {
+/*function getPage (url) {
     const html = cache.templates[url];
 
     if (!html) {
-        return fetch(url).then(function (response) {
+        return fetch(url).then(function(response) {
             return response.text();
         }).then(function (html) {
             cache.templates[url] = html;
             return html;
         });
-    } else {
+    }
+    else {
         return Promise.resolve(html);
     }
+}*/
+
+const pages = [{ id: 'play', 'url': '/play', html: 'site/pages/play.html' }, { id: 'contribute', 'url': '/contribute', html: 'site/pages/contribute.html' }, { id: 'credits', 'url': '/credits', html: 'site/pages/credits.html' }, { id: 'pepperandcarrot', 'url': '/pepperandcarrot', html: 'site/pages/pepperandcarrot.html' }, { id: 'root', 'url': '/', html: 'site/pages/about.html' }];
+
+function deselect() {
+    pages.forEach(function (page) {
+        page.el.className = "hide";
+        page.button.className = page.button.className.replace("selected", "");
+    });
+
+    /*    const el = document.querySelector(".selected");
+        
+        if (el) {
+            el.className = el.className.replace("selected", "");
+        }*/
 }
 
-function open(url) {
-    return getPage(url).then(html => {
-        content.innerHTML = html;
-        gallery();
+function open(el) {
+    el.className += "show";
+}
+
+function setup(page) {
+
+    return fetch(page.html).then(function (response) {
+        return response.text();
+    }).then(function (html) {
+        const el = document.createElement("div");
+        const button = document.getElementById("button-" + page.id);
+
+        el.className = "hide";
+        el.innerHTML = html;
+
+        page.el = el;
+        page.button = button;
+
+        document.getElementById("content").appendChild(el);
+
+        router.on(page.url, function () {
+            deselect();
+
+            button.className += "selected";
+            open(el);
+        }).resolve();
     });
 }
 
+/*
+function open (url) {
+    return getPage(url).then((html) => {
+        content.innerHTML = html;
+        gallery();
+    });
+}*/
+
 // TODO: we need to load pages on DOM, and just hide and show.
 // preload pages,
-['site/pages/play.html', 'site/pages/contribute.html', 'site/pages/credits.html', 'site/pages/pepperandcarrot.html', 'site/pages/about.html'].forEach(getPage);
-
-function deselect() {
+/*
+function deselect () {
     const el = document.querySelector(".selected");
-
+    
     if (el) {
         el.className = el.className.replace("selected", "");
     }
-}
+}*/
 
-router.on("/play", function () {
-    deselect();
+Promise.all(pages.map(setup)).then(gallery);
 
-    document.getElementById("play").className += "selected";
-    open('site/pages/play.html');
-}).on("/contribute", function () {
-    deselect();
+/*
+router
+    .on("/play", function () {
+        deselect();
 
-    document.getElementById("contribute").className += "selected";
-    open('site/pages/contribute.html');
-}).on("/credits", function () {
-    deselect();
+        document.getElementById("play").className += "selected";
+        open('site/pages/play.html');
+    })
+    .on("/contribute", function () {
+        deselect();
 
-    document.getElementById("credits").className += "selected";
-    open('site/pages/credits.html');
-}).on("/pepperandcarrot", function () {
-    deselect();
+        document.getElementById("contribute").className += "selected";
+        open('site/pages/contribute.html');
+    })
+    .on("/credits", function () {
+        deselect();
 
-    document.getElementById("pepperandcarrot").className += "selected";
-    open('site/pages/pepperandcarrot.html');
-}).on("/", function () {
-    deselect();
+        document.getElementById("credits").className += "selected";
+        open('site/pages/credits.html');
+    })
+    .on("/pepperandcarrot", function () {
+        deselect();
 
-    document.getElementById("root").className += "selected";
-    open('site/pages/about.html');
-}).resolve();
+        document.getElementById("pepperandcarrot").className += "selected";
+        open('site/pages/pepperandcarrot.html');
+    })
+    .on("/", function () {
+        deselect();
+        
+        document.getElementById("root").className += "selected";
+        open('site/pages/about.html');
+    })
+    .resolve();
+*/
 
 },{"./gallery":2,"navigo":1}]},{},[3]);
--- site/js/main.js
+++ site/js/main.js
@@ -2,13 +2,13 @@ const Navigo = require("navigo");
 
 const router = new Navigo(null, true, '#!');
 
-const cache = {templates: {}};
+// const cache = {templates: {}};
 
 const content = document.getElementById("content");
 
 const gallery = require("./gallery");
 
-function getPage (url) {
+/*function getPage (url) {
     const html = cache.templates[url];
 
     if (!html) {
@@ -22,34 +22,84 @@ function getPage (url) {
     else {
         return Promise.resolve(html);
     }
+}*/
+
+const pages = [
+    {id: 'play', 'url': '/play', html: 'site/pages/play.html'},
+    {id: 'contribute', 'url': '/contribute', html: 'site/pages/contribute.html'},
+    {id: 'credits', 'url': '/credits', html: 'site/pages/credits.html'},
+    {id: 'pepperandcarrot', 'url': '/pepperandcarrot', html: 'site/pages/pepperandcarrot.html'},
+    {id: 'root', 'url': '/', html: 'site/pages/about.html'}
+];
+
+
+function deselect () {
+    pages.forEach(function (page) {
+       page.el.className = "hide"; 
+       page.button.className = page.button.className.replace("selected", ""); 
+    });
+    
+/*    const el = document.querySelector(".selected");
+    
+    if (el) {
+        el.className = el.className.replace("selected", "");
+    }*/
 }
 
+function open (el) {
+    el.className += "show";
+}
+
+function setup (page) {
+    
+    return fetch(page.html).then(function(response) {
+        return response.text();
+    }).then(function (html) {
+        const el = document.createElement("div");
+        const button = document.getElementById("button-" + page.id);
+
+        el.className = "hide";
+        el.innerHTML = html;
+        
+        page.el = el;
+        page.button = button;
+        
+        document.getElementById("content").appendChild(el);
+        
+        router
+            .on(page.url, function () {
+                deselect();
+        
+                button.className += "selected";
+                open(el);
+            }).resolve();
+    });
+    
+}
+
+/*
 function open (url) {
     return getPage(url).then((html) => {
         content.innerHTML = html;
         gallery();
     });
-}
+}*/
 
 
 // TODO: we need to load pages on DOM, and just hide and show.
 // preload pages,
-[
-    'site/pages/play.html',
-    'site/pages/contribute.html',
-    'site/pages/credits.html',
-    'site/pages/pepperandcarrot.html',
-    'site/pages/about.html'
-].forEach(getPage);
-
+/*
 function deselect () {
     const el = document.querySelector(".selected");
     
     if (el) {
         el.className = el.className.replace("selected", "");
     }
-}
+}*/
 
+Promise.all(pages.map(setup)).then(gallery);
+
+/*
 router
     .on("/play", function () {
         deselect();
@@ -82,5 +132,5 @@ router
         open('site/pages/about.html');
     })
     .resolve();
-
+*/
 
